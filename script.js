9e3c5ff3294ed0a3fe98443501f033edd40149c7
Indraeka <indra.ekaputranta@gmail.com>
2025-07-25 11:22:25 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,4 +18,25 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
+
+    var registrationForm = document.getElementById('registrationForm');
+    if (registrationForm) {
+        registrationForm.addEventListener('submit', function(event) {
+            event.preventDefault();
+            var firstname = registrationForm.querySelector('input[name="firstname"]').value;
+            var lastname = registrationForm.querySelector('input[name="lastname"]').value;
+            var email = registrationForm.querySelector('input[name="email"]').value;
+            // Generate random 7-digit code
+            var confirmationCode = Math.floor(1000000 + Math.random() * 9000000).toString();
+            // Store info in localStorage
+            localStorage.setItem('registration_firstname', firstname);
+            localStorage.setItem('registration_lastname', lastname);
+            localStorage.setItem('registration_email', email);
+            localStorage.setItem('confirmation_code', confirmationCode);
+            // Show code to user
+            alert('Your confirmation code is: ' + confirmationCode + '\nPlease remember this code. You will need it at the end of the intake questionnaire.');
+            // Redirect to questionnaire
+            window.location.href = 'questionnaire.html';
+        });
+    }
 });
